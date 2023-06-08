6a08f95220737111d8aed18dc6f3a18e50cf487c
AzureAD\MelissaLanghausen <melissa.langhausen@lyniate.com>
2023-06-08 09:25:32 -0500

    generic and mapping

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -1,5 +1,6 @@
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastructure.Controllers.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,10 +19,21 @@ namespace Infrastructure.Data
         {
             return await _context.Set<T>().FindAsync(id);
         }
-
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _context.Set<T>().ToListAsync();
+        }
+        public  async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
     }
 }
